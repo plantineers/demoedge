862900b552e45ffa4ef122d0f016369a4a55d4f6
viktoria <v.goennheimer@benz24.de>
2023-04-11 20:41:57 +0200

    Sensor Data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, plants!");
+    let my_data = SensorData{id:"s1".to_string(),sensor_data: Vec::new()};
+}
+struct SensorData{
+    id: String,
+    sensor_data: Vec<Data>
+}
+
+pub struct Data{
+ sensor_type: String,
+ value: f32
 }
