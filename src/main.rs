8dec11a5e01254f0172f52e00eeaa8db93d430a1
viktoria <v.goennheimer@benz24.de>
2023-04-16 21:26:24 +0200

    send data via post to server (hopefully)

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,16 @@ fn main() {
     println!("Hello, plants!");
     let my_data = SensorData{id:"s1".to_string(),sensor_data: Vec::new()};
 }
+//no frcking clue what the data to send looks like / what type it has
+ fn send_data(url:String,data:SensorData){
+    let client = reqwest::Client::new();
+    let res = client.post(url)
+        // .body(data) //this or json
+        .json(data) //creates json from data
+        .send();
+        // .await?;
+}
+
 struct SensorData{
     id: String,
     sensor_data: Vec<Data>
