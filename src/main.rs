18e5b38e0d39639e25e445ca41ed3ae1a810eafa
Bene <37740907+Nereuxofficial@users.noreply.github.com>
2023-04-17 15:28:50 +0200

    Working using OpenWheatherMap

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,73 @@
+use reqwest::get;
 use serde::{Deserialize, Serialize};
+use std::env::args;
+use std::str::FromStr;
 
-fn main() {
-    println!("Hello, plants!");
-    let my_data = SensorData {
-        id: "s1".to_string(),
+#[tokio::main]
+async fn main() {
+    let url = args().nth(1).expect("Please input a url");
+    let (temp, humidity) = get_weather_data(&args().nth(2).expect("Please input an API key")).await;
+    let data = SensorData {
+        id: "test_sensor".to_string(),
         sensor_data: vec![
             Data {
                 r#type: "temperature".to_string(),
-                value: 23.0,
+                value: f32::from_str(&temp).unwrap(),
             },
             Data {
                 r#type: "humidity".to_string(),
-                value: 45.0,
+                value: f32::from_str(&humidity).unwrap(),
             },
         ],
     };
-    let serialized_data = serde_json::to_string(&my_data).unwrap();
-    
+    println!("Sending Data: \n{:?}", data);
+    println!("{}", send_data(url, data).await);
 }
-//no frcking clue what the data to send looks like / what type it has
-async fn send_data(url:String,data:SensorData){
+
+async fn get_weather_data(api_key: &str) -> (String, String) {
+    let weather_data = get(
+        "https://api.openweathermap.org/data/2.5/weather?lat=49.487457&lon=8.466040&appid=API_KEY&units=metric"
+            .replace("API_KEY", api_key),
+    )
+        .await
+        .unwrap()
+        .text()
+        .await.unwrap();
+    // Get "temp":
+    let temp = weather_data
+        .as_str()
+        .split(r#""temp":"#)
+        .nth(1)
+        .unwrap()
+        .split(",")
+        .next()
+        .unwrap();
+    // Get "humidity":
+    let humidity = weather_data
+        .as_str()
+        .split(r#""humidity":"#)
+        .nth(1)
+        .unwrap()
+        .split("}")
+        .next()
+        .unwrap();
+    (temp.into(), humidity.into())
+}
+
+async fn send_data(url: String, data: SensorData) -> reqwest::StatusCode {
     let client = reqwest::Client::new();
-    let res = client.post(url)
+    client
+        .post(url)
         .json(&data) //creates json from data
         .send()
-        // We need to "await" this since we are using asynchronous code, we could also do this in a blocking way
-        // but non-blocking is more performant
-        .await;
+        // We need to "await" this since we are using asynchronous code, we could also do this in a
+        // blocking way but non-blocking is more performant
+        .await
+        .unwrap()
+        .status()
 }
 #[derive(Debug, Serialize, Deserialize)]
-struct SensorData{
+struct SensorData {
     id: String,
     sensor_data: Vec<Data>,
 }
